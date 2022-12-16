29b249d743f10a48a4db14ddb67991604f0a2e94
ibrahimqarayev <qarayevibrahim7@mail.ru>
2022-12-16 22:01:22 +0400

    InputUtil

--- src/utils/InputUtil.java
+++ src/utils/InputUtil.java
@@ -6,14 +6,14 @@ public class InputUtil {
 
     public static String requiredText(String title) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(title+":");
+        System.out.print(title+": ");
         String answer = scanner.nextLine();
         return answer;
     }
 
     public static int requiredNumber(String title) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(title+":");
+        System.out.print(title+": ");
         int answer = scanner.nextInt();
         return answer;
     }
